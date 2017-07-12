483f1cdd7a26f36650f45dd08d5674eff81cae97
wardenlym <wardenlym@gmail.com>
2017-07-12 17:55:52 +0800

    concat

--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"danmaku/syncer"
+	"strings"
 
 	"github.com/garyburd/redigo/redis"
 	"github.com/gin-gonic/gin"
@@ -41,11 +42,20 @@ func (p *App) danmaku_pub(c *gin.Context) {
 }
 
 func (p *App) danmaku_like(c *gin.Context) {
+	var danmakuid, uid, videoid string
+	var like_hset_name string
+	key := concat_string_by("_", videoid, uid, danmakuid)
 	p.syncer_like.SyncRedis(func(conn redis.Conn) {
+		conn.Do("hincrby", like_hset_name, key, 1)
+
 	})
 }
 
 func (p *App) danmaku_dislike(c *gin.Context) {
 	p.syncer_dislike.SyncRedis(func(conn redis.Conn) {
 	})
+}
+
+func concat_string_by(sep string, v ...string) {
+	strings.Join(v, sep)
 }
